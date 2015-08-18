c04d53f61ac2effe8bcc664f32752c4e9525351b
Molly Lewis <mll@stanford.edu>
2015-08-18 14:07:20 -0700

    fixed index link

--- js/index_html.js
+++ js/index_html.js
@@ -3,14 +3,14 @@
 
 $(document).ready(function(){
 
-	var index_top_html = " <li> <a  href='projects/RC/RC_index.html'> Lewis and Frank (under review) </a></li>"
+	var index_top_html = " <li> <a  href='projects/RC/RCindex.html'> Lewis and Frank (under review) </a></li>"
 	index_top_html += " <li> <a  href='projects/xtSamp/xtSamp_index.html'> Lewis and Frank (in prep) </a></li>"
 	index_top_html += " <li> <a href='projects/RCI/RCI_index.html'> Lewis and Frank (2015) </a></li>"
 	index_top_html += " <li> <a href='projects/tablet/tablet_index.html'> Frank et al. (2015) </a></li>"
 
 	$("#index_top").html(index_top_html) 
 
-	var index_bottom_html = " <li> <a  href='../RC/RC_index.html'> Lewis and Frank (under review) </a></li>"
+	var index_bottom_html = " <li> <a  href='../RC/RCindex.html'> Lewis and Frank (under review) </a></li>"
 	index_bottom_html += " <li> <a  href='../xtSamp/xtSamp_index.html'> Lewis and Frank (in prep) </a></li>"
 	index_bottom_html += " <li> <a href='../RCI/RCI_index.html'> Lewis and Frank (2015) </a></li>"
 	index_bottom_html += " <li> <a href='../tablet/tablet_index.html'> Frank et al. (2015) </a></li>"
