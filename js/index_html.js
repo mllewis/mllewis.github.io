ca44c35f4dd7ae4059190b3fc059cae1be5ad233
Molly Lewis <mll@stanford.edu>
2016-06-02 10:31:49 -0700

    update references

--- js/index_html.js
+++ js/index_html.js
@@ -5,14 +5,14 @@ $(document).ready(function(){
 
 	var index_top_html = " <li> <a  href='projects/xtSamp/xtSampindex.html'> Lewis and Frank (in press) </a></li>"
 	index_top_html += " <li> <a  href='projects/RC/RCindex.html'> Lewis and Frank (2016) </a></li>"
+	index_top_html += " <li> <a href='projects/tablet/tablet_index.html'> Frank et al. (2016) </a></li>"
 	index_top_html += " <li> <a href='projects/RCI/RCI_index.html'> Lewis and Frank (2015) </a></li>"
-	index_top_html += " <li> <a href='projects/tablet/tablet_index.html'> Frank et al. (2015) </a></li>"
 
 	$("#index_top").html(index_top_html) 
 	var index_bottom_html = " <li> <a  href='../xtSamp/xtSampindex.html'> Lewis and Frank (in press) </a></li>"
 	index_bottom_html += " <li> <a  href='../RC/RCindex.html'> Lewis and Frank (2016) </a></li>"
+	index_bottom_html += " <li> <a href='../tablet/tablet_index.html'> Frank et al. (2016) </a></li>"
 	index_bottom_html += " <li> <a href='../RCI/RCI_index.html'> Lewis and Frank (2015) </a></li>"
-	index_bottom_html += " <li> <a href='../tablet/tablet_index.html'> Frank et al. (2015) </a></li>"
 
 	$("#index_bottom").html(index_bottom_html) 
 })
